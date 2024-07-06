c278d09f8f5fd53f7b7ec8b09b20a2838809aea1
NaotoCoding <naoto.zaniah@gmail.com>
2024-07-06 18:04:41 +0900

    モデル作成時にhelperを作らないようにした

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module RailsOidcProvider
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
   end
 end
