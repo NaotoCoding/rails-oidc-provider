c9847fa0f1280c958a75cb6168e44a67b4cf1e41
NaotoCoding <naoto.zaniah@gmail.com>
2024-07-06 21:12:16 +0900

    doorkeeper用のcssへのパスを記述

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+//= link doorkeeper/application.css
+//= link doorkeeper/admin/application.css
